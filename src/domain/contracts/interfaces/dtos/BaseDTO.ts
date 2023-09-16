3bfd5f10a48d8cca6e8b5c9efb42873f87f690bc
joedysonbezerra <joedysonbezerra@gmail.com>
2023-09-16 18:24:06 -0300

    fix: Checking if plain is one string

--- src/domain/contracts/interfaces/dtos/BaseDTO.ts
+++ src/domain/contracts/interfaces/dtos/BaseDTO.ts
@@ -2,7 +2,14 @@ import { ValidationError, validateSync } from 'class-validator';
 import { Base } from '@domain/Base';
 
 export class BaseDTO extends Base {
-  static validate<T extends object>(type: new () => T, plain: NonNullable<unknown>): { entity: T; errors: ValidationError[] } {
+  static validate<T extends object>(
+    type: new () => T,
+    plain: NonNullable<unknown> | string
+  ): { entity: T; errors: ValidationError[] } {
+    if (plain instanceof String) {
+      plain = JSON.parse(plain.toString());
+    }
+
     const entity = this.fromPlain(type, plain);
     const validationErrors: ValidationError[] = validateSync(entity);
 
